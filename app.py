05f1cec35764bb32ebcf30be22209e061fb9709e
Daniel BATISTA <109699716+batistadaniel@users.noreply.github.com>
2024-09-19 18:41:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,93 @@
 # Importando a biblioteca
 import pandas as pd
 
-def resultado_final(dados):
+# Classe e suas funcoes
+class ResultadoFinal:
+    def __init__(self, dados):
+        self.dados = dados
+        self.df = None  
 
-    # Separa cada linha de informacoes por (/).
-    # Substitui quebras de linha por espaco e separa por (;).
-    # Armazena em uma lista.
-    colunas = [linha.replace("\n", " ").split(";") for linha in dados.split('/')]
+    def resultado_final(self):
+        
+        # Separa cada linha de informacoes por (/).
+        # Substitui quebras de linha por espaco e separa por (;).
+        # Armazena em uma lista.        
+        colunas = [linha.replace("\n", " ").split(";") for linha in self.dados.split('/')]
 
-    # Cria o DataFrame com as colunas definidas.
-    df = pd.DataFrame(colunas, columns=['Nome', 'Inscricao', 'Pontuacao', 'Classificacao Micro-Macrorregiao', 'PCD', 'PPP'])
+        # Cria o DataFrame
+        self.df = pd.DataFrame(colunas, columns=['Nome', 'Inscricao', 'Pontuacao', 'Classificacao Micro-Macrorregiao', 'PCD', 'PPP']).set_index('Nome')
 
-    # Ordena o DataFrame pela coluna Pontuacao em ordem decrescente para classificacao ficar crescente, porem como existem notas repitidas tive que reordenar no google sheets novamente.
-    df = df.sort_values(by='Pontuacao', ascending=False)
+        # Convertendo a classificacao geral para numérico
+        self.df['Classificacao Micro-Macrorregiao'] = pd.to_numeric(self.df['Classificacao Micro-Macrorregiao'], errors='coerce')
 
-    # Define a coluna Nome como indice do DataFrame.
-    csv = df.set_index('Nome')
+        # Ordenando de forma crescente a coluna 'Classificacao Micro-Macrorregiao'
+        self.df = self.df.sort_values(by='Classificacao Micro-Macrorregiao', ascending=True)
 
-    # Exibe o DataFrame.
-    display(csv)
+        # Exibe o DataFrame
+        display(self.df)
+
+    def ultima_pessoa_sem_cota(self):
+        if self.df is not None:  # Verifica se df foi gerado
+            # Filtra a última linha onde 'PCD' e 'PPP' são iguais a " - "
+            filtro = self.df[(self.df['PCD'] == " - ") & (self.df['PPP'] == " - ")].tail(1)
+
+            # Exibe o DataFrame filtrado
+            display(filtro)
+        else:
+            print("DataFrame não gerado. Execute o método resultado_final primeiro.")
+
+    def ultima_pessoa_cota_pcd(self):
+        if self.df is not None:  # Verifica se df foi gerado
+            # Converte a coluna 'PCD' para numérico, onde valores inválidos se tornam NaN
+            self.df['PCD'] = pd.to_numeric(self.df['PCD'], errors='coerce')
+
+            # Filtra a última pessoa onde 'PCD' tem um valor numérico 
+            filtro = self.df[self.df['PCD'].notna()].tail(1)
+
+            # Exibe o DataFrame filtrado
+            display(filtro)
+        else:
+            print("DataFrame não gerado. Execute o método resultado_final primeiro.")
 
 
-resultado_final("""Aarao Aata Leal Guimaraes; 571190589-7; 74,5; 905 ; - ; - /Abel Barbosa Neto Souto; 571022340-0; 75,0; 850 ; - ; -
+    def ultima_pessoa_cota_ppp(self):
+        if self.df is not None:  # Verifica se df foi gerado
+            # Converte a coluna 'PPP' para numérico, onde valores inválidos se tornam NaN
+            self.df['PPP'] = pd.to_numeric(self.df['PPP'], errors='coerce')
+
+            # Filtra a última pessoa onde 'PPP' tem um valor numérico 
+            filtro = self.df[self.df['PPP'].notna()].tail(1)
+            # Exibie o DataFrame filtrado
+            display(filtro)
+        else:
+            print("DataFrame não gerado. Execute o método resultado_final primeiro.")
+
+
+
+
+# Atribui os dados a uma variavel
+dados = """Aarao Aata Leal Guimaraes; 571190589-7; 74,5; 905 ; - ; - /Abel Barbosa Neto Souto; 571022340-0; 75,0; 850 ; - ; -
 /Abimael de Queiroz Lima; 570068440-7; 66,0; 3367 ; - ; 241 /Abner Filipe Cunha Ribeiro; 572090305-5; 72,5; 1279 ; - ; -
 /Abner Silveira de Freitas; 571925980-4; 73,5; 1055 ; - ; - /Abraao Freitas Medeiros; 571460406-8; 76,0; 679 ; - ; - /Aciria
 Borges Alves Rufino; 570525714-0; 73,0; 1177 ; - ; - /Adalberto Luis Navarrete Filho; 570925900-3; 88,0; 15 ; - ; - /Adam
-Basilio; 570498450-4; 74,5; 947 ; - ; -""")
+Basilio; 570498450-4; 74,5; 947 ; - ; -"""
+
+
+# Instancia a classe
+resultado = ResultadoFinal(dados)
+
+
+# Funcao que exibe o resultado final completo
+resultado.resultado_final()
+
+
+# Funcao que exibe informacoes sobre o corte para ampla concorrencia
+resultado.ultima_pessoa_sem_cota()
+
+
+# Funcao que exibe informacoes sobre o corte para vagas pcd
+resultado.ultima_pessoa_cota_pcd()
+
+
+# Funcao que exibe informacoes sobre o corte para vagas ppp
+resultado.ultima_pessoa_cota_ppp()
